ed47d5cfa3cbeeedb513053361ab9d4964e7e009
Kenji Katahira <kkatahira@uolinc.com>
2020-12-14 11:01:02 -0300

    ✍ - updating examples page

--- examples/App.js
+++ examples/App.js
@@ -90,7 +90,7 @@ const App = () => {
 		let mapPos = [latitude,longitude];
 		return (
 			<>
-				<MapContainer center={mapPos} zoom={13} scrollWheelZoom={false}>
+				<MapContainer center={mapPos} zoom={12} scrollWheelZoom={false}>
 					<TileLayer
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
